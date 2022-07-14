18c991e633af0fb0448941ec8f29560b1dc8a0c9
Damian Barrous-Dume <damianbarrous@gmail.com>
2022-07-14 19:28:13 +0100

    Modify cdk template

--- app.py
+++ app.py
@@ -7,22 +7,10 @@ from sdc_aws_processing_lambda.sdc_aws_processing_lambda import SDCAWSProcessing
 
 
 app = cdk.App()
-SDCAWSProcessingLambdaStack(app, "SDCAWSProcessingLambdaStack",
-    # If you don't specify 'env', this stack will be environment-agnostic.
-    # Account/Region-dependent features and context lookups will not work,
-    # but a single synthesized template can be deployed anywhere.
-
-    # Uncomment the next line to specialize this stack for the AWS Account
-    # and Region that are implied by the current CLI configuration.
-
-    #env=cdk.Environment(account=os.getenv('CDK_DEFAULT_ACCOUNT'), region=os.getenv('CDK_DEFAULT_REGION')),
-
-    # Uncomment the next line if you know exactly what Account and Region you
-    # want to deploy the stack to. */
-
-    #env=cdk.Environment(account='123456789012', region='us-east-1'),
-
-    # For more information, see https://docs.aws.amazon.com/cdk/latest/guide/environments.html
+SDCAWSProcessingLambdaStack(app, "SDCAWSProcessingLambdaStack", env=cdk.Environment(
+        account=os.getenv('CDK_DEFAULT_ACCOUNT'),
+        region='us-east-2'
     )
+)
 
 app.synth()
--- sdc_aws_processing_lambda/sdc_aws_processing_lambda.py
+++ sdc_aws_processing_lambda/sdc_aws_processing_lambda.py
@@ -1,6 +1,8 @@
 from aws_cdk import (
     Stack,
     aws_lambda,
+    aws_ecr,
+    aws_ecr_assets,
     aws_iam as iam,
 )
 
@@ -11,20 +13,16 @@ class SDCAWSProcessingLambdaStack(Stack):
    def __init__(self, scope: Construct, construct_id: str, **kwargs) -> None:
         super().__init__(scope, construct_id, **kwargs)
 
-        # Create IAM Lambda execution role
-        lambda_exec_role = iam.Role(
-            self,
-            'swsoc-lambda-processing-function',
-            assumed_by=iam.ServicePrincipal('lambda.amazonaws.com'),
-            role_name='swsoc-lambda-processing-function',
-            description='Role used by SWSOC Processing Lambda Function.',
-            managed_policies=[
-                iam.ManagedPolicy.from_managed_policy_arn(self,
-                    'admin_role',
-                    'arn:aws:iam::aws:policy/AdministratorAccess'
-                 )
-            ]
-        )
+        ecr_repository = aws_ecr.Repository(
+                            self, 
+                            id="Repo", 
+                            repository_name="sdc_aws_processing_lambda_function",
+                            )
+
+        docker_image_asset = aws_ecr_assets.DockerImageAsset(self, "sdc_aws_processing_lambda",
+            directory="./sdc_aws_processing_lambda/assets",
+            file="Dockerfile"
+            )
 
         ### Create Cognito Remediator Lambda function
         sdc_aws_processing_function = aws_lambda.DockerImageFunction(
@@ -32,7 +30,7 @@ class SDCAWSProcessingLambdaStack(Stack):
             id="sdc_aws_processing_lambda_function",
             function_name="sdc_aws_processing_lambda_function",
             description="SWSOC Processing Lambda function deployed using AWS CDK Python",
-            code=aws_lambda.DockerImageCode.from_image_asset("./sdc_aws_processing_lambda/assets/"),
+            code=aws_lambda.DockerImageCode.from_ecr(ecr_repository),
         )
 
 

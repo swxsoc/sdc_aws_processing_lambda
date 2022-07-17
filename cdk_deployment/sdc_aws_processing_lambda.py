2006b775792c18e19337977657da137d32ffcbb6
Damian Barrous-Dume <damianbarrous@gmail.com>
2022-07-17 17:07:08 +0100

    Revert "Add CDK and CI/CD Pipeline files"
    This reverts commit 33eafd03db6644e4df3277a049905b84fe36a4a9.

--- cdk_deployment/sdc_aws_processing_lambda.py
+++ cdk_deployment/sdc_aws_processing_lambda.py
@@ -4,19 +4,30 @@ from constructs import Construct
 import logging
 
 
-class SortingLambdaStack(Stack):
+class SDCAWSProcessingLambdaStack(Stack):
     def __init__(self, scope: Construct, construct_id: str, **kwargs) -> None:
         super().__init__(scope, construct_id, **kwargs)
+
+        # ECR Repo Name
+        repo_name = "sdc_aws_processing_lambda"
+
+        # Get SDC Processing Lambda ECR Repo
+        ecr_repository = aws_ecr.Repository.from_repository_name(
+            self, id=f"{repo_name}_repo", repository_name=repo_name
+        )
+
+        # Get time tag enviromental variable
+        TAG = os.getenv("TAG") if os.getenv("TAG") is not None else "latest"
 
         # Create Container Image ECR Function
         sdc_aws_processing_function = aws_lambda.DockerImageFunction(
             scope=self,
-            id="sdc_aws_sorting_lambda_function",
-            function_name="sdc_aws_sorting_lambda_function",
+            id=f"{repo_name}_function",
+            function_name=f"{repo_name}_function",
             description=(
                 "SWSOC Processing Lambda function deployed using AWS CDK Python"
             ),
-            code=aws_lambda.AssetCode("lambda_function"),
+            code=aws_lambda.DockerImageCode.from_ecr(ecr_repository, tag_or_digest=TAG),
         )
 
         logging.info("Function created successfully: %s", sdc_aws_processing_function)
